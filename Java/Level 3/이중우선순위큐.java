6807acec3072c97d3fa6fbf1e1d4ff7f8fa84eaf
귤까먹는 코끼리 개발자 <82919411+elephant97@users.noreply.github.com>
2023-11-24 12:23:40 +0900

    Update 이중우선순위큐.java
    필요없는 연산들 삭제 및 import 함수 오타 수정

--- Java/Level 3/이중우선순위큐.java
+++ Java/Level 3/이중우선순위큐.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 
 
 class Solution {
@@ -7,15 +7,16 @@ class Solution {
         int[] answer = {0,0};
         int max_del = 0;
         int min_del = 0;
-        int list_size = 0;
-        ArrayList<Integer> queue = new ArrayList<Integer>();
+        int list_size = 0
+        
+        ArrayList<Integer> queue = new ArrayList<Integer>();//ArrayList는 크기 지정 없이 add하면 자동으로 크기가 늘어남
         
         for(int i = 0; i < operations.length; i++)
         {
             String[] splitOper = operations[i].split(" ");
-            if(splitOper[0].charAt(0) == 'I')
+            if(splitOper[0].charAt(0) == 'I') //0번째 배열에서 0번째 값을 charactor로 접근 할 때 charAt(0)으로 
             {
-                queue.add(Integer.parseInt(splitOper[1]));
+                queue.add(Integer.parseInt(splitOper[1]));//1번째 배열의 String 값을 인티저로 변환
             }
             else if(!queue.isEmpty() && splitOper[0].charAt(0) == 'D')
             {
@@ -26,8 +27,6 @@ class Solution {
             }      
         }
         
-        Collections.sort(queue);
-        list_size = queue.size();
         
         if(!queue.isEmpty())
         {
