5c2e7eb025e87dbcd07516049e8aefa3e41c93ed
귤까먹는 코끼리 개발자 <82919411+elephant97@users.noreply.github.com>
2023-11-27 12:56:18 +0900

    Update 네트워크.java

--- Java/Level 3/네트워크.java
+++ Java/Level 3/네트워크.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        int [][]visit = new int[n][n];
+        //int [][]visit = new int[n][n]; // 굳이 2중배열이 아니라 일차원 배열로 해서 해당 행 체크했는지만 체크하면 됨
+        boolean []visit = new boolean[n];
         
         for(int i = 0; i < computers.length; i++){
             for(int j = 0; j < computers[i].length; j++){
-                if(visit[i][j] == 0 && computers[i][j] == 1)
+                if(visit[i] == false && computers[i][j] == 1)
                 {
                     dfs(computers, visit, j);
                     answer++;
@@ -16,14 +17,15 @@ class Solution {
         return answer;
     }
     
-    public void dfs(int[][] computers, int[][] visit, int node){
+    public void dfs(int[][] computers, boolean[] visit, int node){
+        visit[node] = true;
         
         for(int j = 0; j < computers.length; j++){
-            if(computers[node][j] == 1 && visit[node][j] == 0){
-                visit[node][j] = 1;
-                dfs(computers, visit, j);
+            if(computers[node][j] == 1 && visit[j] == false){
+                dfs(computers, visit, j); 
             }
         }
     }
     
+   
 }
