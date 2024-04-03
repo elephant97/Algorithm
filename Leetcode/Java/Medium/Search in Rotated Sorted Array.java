15a13f6dd16c682bb8433eae91d87a57b9dfbfce
귤까먹는 코끼리 개발자 <82919411+elephant97@users.noreply.github.com>
2024-04-04 00:24:55 +0900

    Update Search in Rotated Sorted Array.java

--- Leetcode/Java/Medium/Search in Rotated Sorted Array.java
+++ Leetcode/Java/Medium/Search in Rotated Sorted Array.java
@@ -1,32 +1,54 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            
-            if (nums[mid] == target) {
-                return mid;
+        int numsLength = nums.length;
+        int start = 0;
+        int end = numsLength-1;
+        int ans = -1;
+
+        if(numsLength == 1){
+            return nums[0] == target? 0 : -1;
+        }
+
+        while(end - start > 0){            
+            int half = (end - start) / 2;
+
+            if(nums[half] == target){
+                ans = half;
+                break;
             }
-            
-            
-            if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+            if(nums[start] == target){
+                ans = start;
+                break;
+            }
+            if(nums[end] == target){
+                ans = end;
+                break;
+            }
+
+            if(nums[start] <= target && nums[start+half] >= target){
+                end = start+half;
+            }else if(nums[start+half+1] <= target && nums[end] >= target){
+                start = start+half+1;
+            }else{
+                if(nums[half+start] >= target && nums[end] >= target){
+                    if(nums[end] - target < nums[half+start] - target){
+                        start = half+start+1;
+                    }else{
+                        end = start+half;
+                    }
+                }else {
+                    if(target - nums[start] > target - nums[start+half+1]){
+                        start = start+half+1;
+                    }else{
+                        end = start+half;
+                    }
                 }
-            }             
-            else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+
+                if(end > numsLength-1) end = numsLength-1;
+               
             }
         }
-        
-        return -1; 
-    }
 
+        return ans;
+    }
 }
