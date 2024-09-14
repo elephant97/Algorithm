073f07adef4ef334c1ee5b8bab9320e346887df3
귤까먹는 코끼리 개발자 <82919411+elephant97@users.noreply.github.com>
2024-09-15 02:18:34 +0900

    Update 4sum.java

--- Leetcode/Java/Medium/4sum.java
+++ Leetcode/Java/Medium/4sum.java
@@ -1,37 +1,42 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int k = 0; k < nums.length; k++) {
+            
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
 
-        List<List<Integer>> answerLists = new ArrayList<>();
-        
-        
+            for (int i = k + 1; i < nums.length; i++) {
+                
+                if (i > k + 1 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
 
+                int left = i + 1;
+                int right = nums.length - 1;
 
-        // for(int i = 0; i < (nums.length-1)/ 2 -1; i++)
-        // {  
-            int left = 0;
-            int right = nums.length-1;
-            int leftMove = 1;
-            int rightMove = 1;  
-            while(left+leftMove < right-rightMove){
-                int sum = nums[left]+nums[left+leftMove]+nums[right-rightMove]+nums[right];
+                while (right > left) {
+                    long sum = (long) nums[k] + nums[i] + nums[left] + nums[right];
+                    if (sum > target) {
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        result.add(Arrays.asList(nums[k], nums[i], nums[left], nums[right]));
 
-                if(target == sum){
-                    List<Integer> sumList = new ArrayList<>();
-                    sumList.add(nums[left]);
-                    sumList.add(nums[left+leftMove]);
-                    sumList.add(nums[right-rightMove]);
-                    sumList.add(nums[right]);
-                    answerLists.add(sumList);
-                    leftMove++;
-                    rightMove++;
-                }else if(target > sum){
-                    right--;
-                }else{
-                    left++;
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (right > left && nums[left] == nums[left + 1]) left++;
+
+                        right--;
+                        left++;
+                    }
                 }
             }
-        // }
-        return answerLists;
+        }
+        
+        return result;
     }
+    
 }
